1722e4b2dd97708eeef5ebda375ad0259907e1b4
Noobeggs <chiewyanwei@gmail.com>
2023-10-07 17:10:50 +0800

    Oops! Forgot to track this file. :P
    (cherry picked from commit 9ca9c416c97eb9348470d70cab219c4956ee1842)

---
+++ src/commands/llama.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+use crate::Error;
+use crate::Data;
+use crate::singletons::HTTP_CLIENT;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct LlamaResponse {
+    response: String
+}
+
+#[poise::command(prefix_command)]
+pub async fn llama(
+    ctx: poise::Context<'_, Data, Error>,
+    message: String
+) -> Result<(), Error> {
+    let mut map = HashMap::new();
+    map.insert("chat", message);
+
+    let res: LlamaResponse = HTTP_CLIENT.post(ctx.data().secrets.get("llama_url").unwrap())
+        .json(&map)
+        .send()
+        .await?
+        .json()
+        .await?;
+    
+    ctx.send(|b| {
+        b.content(res.response)
+            .reply(true)
+    })
+    .await?;
+    Ok(())
+}
