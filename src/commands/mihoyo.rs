e662d0fc9e14c5eefe429a47b597104254e79f01
Noobeggs <chiewyanwei@gmail.com>
2023-10-07 17:12:02 +0800

    Formatting.

--- src/commands/mihoyo.rs
+++ src/commands/mihoyo.rs
@@ -1,7 +1,7 @@
 use crate::Error;
 use crate::Data;
 use poise::serenity_prelude as serenit;
-use serenity::utils::{Colour};
+use serenity::utils::Colour;
 
 #[poise::command(slash_command)]
 pub async fn genshin_codes(
